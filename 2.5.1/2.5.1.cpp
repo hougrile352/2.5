a25db20a5fd7cfd757420ce3a428848f85719253
Riley Houghton <HougRile352@GoTVDSB.ca>
2021-11-24 09:07:49 -0500

    Both assignments done ~ change the value of "autocap" to switch between 2.5.1 and 2.5.2

--- 2.5.1/2.5.1.cpp
+++ 2.5.1/2.5.1.cpp
@@ -2,11 +2,38 @@
 //
 
 #include <iostream>
+#include <string> //libraries and namespace
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string name; //Initialize the name string
+    int autocap = 1; //Controls automatic capitalization ~ set to 0 to disable
+    cout << "Enter string!\n"; //Prompt user for string
+    try {
+        cin >> name; //Enter the string
+        if (islower(name[0])) //Check if lowercase
+        {
+            throw "Name not uppercase"; //Throw exception if first letter is lowercase
+        }
+        cout << "First letter is capitalized\n"; //Print this if first letter is capitalized
+    }
+
+    catch (...)
+    {
+        cout << "First letter not capitalized!\n"; //Warn user first letter is lowercase
+        if (autocap == 1) //Check for auto capitalization setting
+        {
+            name[0] = (toupper(name[0])); //Capitalize the first letter
+            cout << "First letter successfully capitalized.\n";
+        }
+
+    }
+
+    cout << "Your string is: \n" << name; //Print string
 }
+
+    
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
